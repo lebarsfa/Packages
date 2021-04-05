2e59971b0a282b6ef28a1541e8fcf66e95aec738
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-04-05 20:38:50 +0200

    Improved 32 bit compatibility

--- choco/qt5-default/tools/chocolateyinstall.ps1
+++ choco/qt5-default/tools/chocolateyinstall.ps1
@@ -1,7 +1,15 @@
 ﻿# Source variables which are shared between install and uninstall.
 . $PSScriptRoot\sharedVars.ps1
 
-cmd /c "refreshenv & pip install --only-binary :all: aqtinstall==$aqtVer & aqt install --outputdir $QtSDKRoot $QtSDKVer windows desktop win$MinGWArch`_mingw$MinGWMMVer -m qtcharts qtscript"
+# Ideally...
+#cmd /c "refreshenv & pip install --only-binary :all: aqtinstall==$aqtVer & aqt install --outputdir $QtSDKRoot $QtSDKVer windows desktop win$MinGWArch`_mingw$MinGWMMVer -m qtcharts qtscript"
+
+# If some py7zr pip dependencies fail...
+# Would need also 7zip and git as package dependencies...
+cmd /c "refreshenv & cd %TMP% & git -c advice.detachedHead=false clone --depth 1 -b v$aqtVer https://github.com/miurahr/aqtinstall"
+((Get-Content -path "$env:TMP\aqtinstall\setup.cfg" -Raw) -replace "py7zr","#py7zr") | Set-Content -Path "$env:TMP\aqtinstall\setup.cfg"
+((Get-Content -path "$env:TMP\aqtinstall\aqt\installer.py" -Raw) -replace "import py7zr","#import py7zr") | Set-Content -Path "$env:TMP\aqtinstall\aqt\installer.py"
+cmd /c "refreshenv & pip install --only-binary :all: -e %TMP%\aqtinstall & aqt install -E 7z --outputdir $QtSDKRoot $QtSDKVer windows desktop win$MinGWArch`_mingw$MinGWMMVer -m qtcharts qtscript"
 
 $QtVersionFileContent = @" 
 <qtcreator>
