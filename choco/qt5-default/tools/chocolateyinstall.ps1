89f52ba72cf13f0d1384339276d52c1de76f7d5b
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-05-05 00:33:32 +0200

    Updated aqtinstall version

--- choco/qt5-default/tools/chocolateyinstall.ps1
+++ choco/qt5-default/tools/chocolateyinstall.ps1
@@ -5,11 +5,8 @@
 #cmd /c "refreshenv & pip install --only-binary :all: aqtinstall==$aqtVer & aqt install --outputdir $QtSDKRoot $QtSDKVer windows desktop win$MinGWArch`_mingw$MinGWMMVer -m qtcharts qtscript"
 
 # If some py7zr pip dependencies fail...
-# Would need also 7zip and git as package dependencies...
-cmd /c "refreshenv & cd %TMP% & git -c advice.detachedHead=false clone --depth 1 -b v$aqtVer https://github.com/miurahr/aqtinstall"
-((Get-Content -path "$env:TMP\aqtinstall\setup.cfg" -Raw) -replace "py7zr","#py7zr") | Set-Content -Path "$env:TMP\aqtinstall\setup.cfg"
-((Get-Content -path "$env:TMP\aqtinstall\aqt\installer.py" -Raw) -replace "import py7zr","#import py7zr") | Set-Content -Path "$env:TMP\aqtinstall\aqt\installer.py"
-cmd /c "refreshenv & pip install --only-binary :all: -e %TMP%\aqtinstall & aqt install -E 7z --outputdir $QtSDKRoot $QtSDKVer windows desktop win$MinGWArch`_mingw$MinGWMMVer -m qtcharts qtscript"
+# Would need also 7zip...
+cmd /c "refreshenv & pip install --only-binary :all: requests==$requestsVer packaging texttable & pip install --only-binary :all: --no-deps aqtinstall==$aqtVer & aqt install -E 7z --outputdir $QtSDKRoot $QtSDKVer windows desktop win$MinGWArch`_mingw$MinGWMMVer -m qtcharts qtscript"
 
 $QtVersionFileContent = @" 
 <qtcreator>
--- choco/qt5-default/tools/sharedVars.ps1
+++ choco/qt5-default/tools/sharedVars.ps1
@@ -1,6 +1,7 @@
+$requestsVer = "2.25.1"
+$aqtVer = "1.1.6"
 if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq $true) { $MinGWArch = "32" } else { $MinGWArch = "64" }
 $MinGWMMVer = "81"
-$aqtVer = "1.1.4"
 $QtSDKVer = "5.15.2"
 $QtSDKRoot = "C:\Qt"
 $QtSDKPath = "$QtSDKRoot\$QtSDKVer"
