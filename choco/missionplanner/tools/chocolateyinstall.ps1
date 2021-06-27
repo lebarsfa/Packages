1c595261021bb9289c1422faf0f299d55c1e8f39
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-06-27 16:55:08 +0200

    Updated missionplanner package

--- choco/missionplanner/tools/chocolateyinstall.ps1
+++ choco/missionplanner/tools/chocolateyinstall.ps1
@@ -1,23 +1,39 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Get-Item $toolsDir\*.msi
+$fileLocation = Get-Item $toolsDir\*.zip
+
+$pp = Get-PackageParameters
+$installDir = $toolsDir
+if ($pp.InstallDir -or $pp.InstallationPath ) { $InstallDir = $pp.InstallDir + $pp.InstallationPath }
+Write-Host "Mission Planner is going to be installed in '$installDir'"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType       = 'MSI'
-  file           = $fileLocation
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  softwareName   = 'Mission Planner*'
+  unzipLocation = "$toolsDir\$env:ChocolateyPackageName"
+  file          = $fileLocation
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
 
-Remove-Item $toolsDir\*.msi -ea 0 -force
+Remove-Item $toolsDir\*.zip -ea 0 -force
 
-$installLocation = Get-AppInstallLocation $packageArgs.softwareName
-if (!$installLocation) { Write-Warning "Can't find Mission Planner install location"; return }
-Write-Host "Mission Planner installed to '$installLocation'"
+$MissionPlanner = $null
 
-Install-BinFile 'MissionPlanner' $installLocation\MissionPlanner.exe
+Get-ChildItem $packageArgs.unzipLocation -Include "*.exe" -Recurse | ForEach-Object {
+  if ($_.Name -eq "MissionPlanner.exe") {
+    Set-Content -Value "" -LiteralPath "$($_.FullName).gui"
+    $MissionPlanner = $_.FullName
+  }
+  else {
+    Set-Content -Value "" -LiteralPath "$($_.FullName).ignore"
+  }
+}
+
+if ($MissionPlanner) {
+  # Because chocolatey targets 4.0, we are able to use 'Programs' in the 'GetFolderPath'
+  $programs = [System.Environment]::GetFolderPath("Programs")
+  Install-ChocolateyShortcut -shortcutFile "$programs\Mission Planner.lnk" -targetPath $MissionPlanner
+} else {
+  Write-Warning "Unable to find Mission Planner executable, skipping start menu shortcut creation!"
+}
--- choco/missionplanner/tools/chocolateyuninstall.ps1
+++ choco/missionplanner/tools/chocolateyuninstall.ps1
@@ -1,41 +1,7 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'Mission Planner*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  fileType      = 'MSI'
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
-}
-
-$uninstalled = $false
-
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
-
-if ($key.Count -eq 1) {
-  $key | % { 
-    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
-    
-    if ($packageArgs['fileType'] -eq 'MSI') {
-      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-      $packageArgs['file'] = ''
-    } else {
-      # NOTES:
-      # - You probably will need to sanitize $packageArgs['file'] as it comes from the registry and could be in a variety of fun but unusable formats
-      # - Split args from exe in $packageArgs['file'] and pass those args through $packageArgs['silentArgs'] or ignore them
-      # - Ensure you don't pass double quotes in $file (aka $packageArgs['file']) - otherwise you will get "Illegal characters in path when you attempt to run this"
-      # - Review the code for auto-uninstaller for all of the fun things it does in sanitizing - https://github.com/chocolatey/choco/blob/bfe351b7d10c798014efe4bfbb100b171db25099/src/chocolatey/infrastructure.app/services/AutomaticUninstallerService.cs#L142-L192
-    }
-
-    Uninstall-ChocolateyPackage @packageArgs
-
-	Uninstall-BinFile -Name "MissionPlanner.exe" -Path $packageArgs["file"]
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+# Remove the start menu shortcut
+$startMenu = [System.Environment]::GetFolderPath('Programs')
+if (Test-Path "$startMenu\Mission Planner.lnk") {
+  Remove-Item -Force "$startMenu\Mission Planner.lnk"
 }
