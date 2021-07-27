df2b6b5357e30fee4482812533b8f697a70f164b
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-07-27 22:10:59 +0200

    Added ros-melodic-ibex, ros-melodic-codac, ros-noetic-ibex and ros-noetic-codac

---
+++ choco/ros-melodic-codac/tools/setup.bat
@@ -0,0 +1,4 @@
+set "ROS_DISTRO=melodic" 
+set "ROS_ETC_DIR=c:\opt\ros\melodic\x64\/etc/ros" 
+set "PYTHONHOME=" 
+set "ROS_PYTHON_VERSION=3" 
---
+++ choco/ros-noetic-codac/tools/setup.bat
@@ -0,0 +1,4 @@
+set "ROS_DISTRO=noetic" 
+set "ROS_ETC_DIR=c:\opt\ros\noetic\x64\/etc/ros" 
+set "PYTHONHOME=" 
+set "ROS_PYTHON_VERSION=3" 
