ff474662271f8a6aa5300f01a45c10722523f700
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-09-18 20:11:43 +0200

    Updated libopencv-dev package

--- choco/libopencv-dev/tools/sharedVars.ps1
+++ choco/libopencv-dev/tools/sharedVars.ps1
@@ -1,7 +1,7 @@
 if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq $true) { $MinGWArch = "x86" } else { $MinGWArch = "x64" }
 $MinGWMVer = "8"
 $VSVer = "15"
-$OpenCVVer = "3.2.0"
+$OpenCVVer = "4.2.0"
 $CMakeRegistryPath = "HKCU:\SOFTWARE\Kitware\CMake"
 $CMakeSystemRepositoryPath = "HKLM:\SOFTWARE\Kitware\CMake\Packages"
 $CMakePackageName = "OpenCV"
