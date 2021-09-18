d625cf1899875ab0161ac6c5a82ffd74647254bd
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2021-09-19 00:02:18 +0200

    Updated libopencv-dev package

---
+++ choco/qt5-default/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'; # Stop on all errors.
+
+# Source variables which are shared between install and uninstall.
+. $PSScriptRoot\sharedVars.ps1
+
+if (Test-Path $QtSDKMinGWPath) {
+    Remove-Item -Recurse -Force $QtSDKMinGWPath
+}
