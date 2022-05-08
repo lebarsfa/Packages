2b5a8d6fb008f44f6651b88b15d0d20053a989ae
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2022-05-08 17:31:56 +0200

    Update aqt package

--- choco/aqt/tools/chocolateyInstall.ps1
+++ choco/aqt/tools/chocolateyInstall.ps1
@@ -1,20 +1,18 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$url        = 'https://github.com/miurahr/aqtinstall/releases/download/v$env:ChocolateyPackageVersion/aqt_x86.exe'
-$url        = 'https://github.com/lebarsfa/aqtinstall/releases/download/master/aqt_x86.exe'
-#$url64      = 'https://github.com/miurahr/aqtinstall/releases/download/v$env:ChocolateyPackageVersion/aqt_x64.exe'
-$url64      = 'https://github.com/lebarsfa/aqtinstall/releases/download/master/aqt_x64.exe'
+$url        = "https://github.com/miurahr/aqtinstall/releases/download/v$env:ChocolateyPackageVersion/aqt_x86.exe"
+$url64      = "https://github.com/miurahr/aqtinstall/releases/download/v$env:ChocolateyPackageVersion/aqt_x64.exe"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\aqt.exe"
   url           = $url
   url64bit      = $url64
-  checksum      = '249b4c978b6c3ea6a2d2bdb3ab2d4f537689000a77af0360e48f6d58dc31da02'
+  checksum      = 'B980985CFEB1CEFEF1D2EBAB91AE4E648873A27FAE8831DEFF8144661480C56A'
   checksumType  = 'sha256'
-  checksum64    = 'a741098fcf2d57a5607b461fded3dd808690b721e676354f633512d5e89af482'
+  checksum64    = 'B0AD07FE8FD2C094425449F3053598959E467833DADF509DA948571259510078'
   checksumType64= 'sha256'
-  forceDownload = True
+  forceDownload = $true
 }
 
 Get-ChocolateyWebFile @packageArgs
