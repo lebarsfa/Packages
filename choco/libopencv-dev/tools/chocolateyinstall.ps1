317c14ab779a067744f478f1b3b4be591b710db0
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2022-08-10 22:00:53 +0200

    Update libopencv-dev.4.5.4 package

--- choco/libopencv-dev/tools/chocolateyinstall.ps1
+++ choco/libopencv-dev/tools/chocolateyinstall.ps1
@@ -17,9 +17,9 @@ New-Item -ItemType Directory -Force -Path $root | Out-Null
 
 if (!$pp['url']) { 
 	$url = 'https://github.com/lebarsfa/Packages/releases/download/libopencv-dev.4.5.4.20220805/libopencv-dev.4.5.4_x86_mingw11_staticlib_Release.exe'
-	$checksum = '9AF9A6B410C20DA398E0F619D70D2E3FE9F7DFCD983895BEB8BF3039D43BC3E1'
+	$checksum = '7855B0393DF389855E126E712C51DAB8649F19BE0101579C753FBEDF108B0132'
 	$url64 = 'https://github.com/lebarsfa/Packages/releases/download/libopencv-dev.4.5.4.20220805/libopencv-dev.4.5.4_x64_mingw11_staticlib_Release.exe'
-	$checksum64 = 'EA698712B1955870E3AE93F16A23C7BEF7411719D2BBC4E78381809F994CCD0D'
+	$checksum64 = '89C6BB0DE09BD86524947827B3ED88CBBDCAB63141AEE5F7FF273CFB416C0D22'
 	if ($pp['32and64']) {
 		$packageArgs = @{
 			packageName   = $env:ChocolateyPackageName
@@ -54,9 +54,9 @@ if (!$pp['url']) {
 	}
 
 	$url = 'https://github.com/lebarsfa/Packages/releases/download/libopencv-dev.4.5.4.20220805/libopencv-dev.4.5.4_x86_mingw11_lib_Release.exe'
-	$checksum = '4C60BF460A857A4996B29CF7DA51B3956796542D88B845542D32B8F25D5041F4'
+	$checksum = '1B991766F325A8E34A126AF87BB6DDE12467491DA34CE65AEF3D53F85CA00850'
 	$url64 = 'https://github.com/lebarsfa/Packages/releases/download/libopencv-dev.4.5.4.20220805/libopencv-dev.4.5.4_x64_mingw11_lib_Release.exe'
-	$checksum64 = 'A89C7CBDCD1C611457A331161D2D535A2A62E1BE78DC61F2D018D4885839C836'
+	$checksum64 = '25DDC47CD16596C08BB158B13CF60CFDB7A7A200286ACA5870BB196C35877942'
 	if ($pp['32and64']) {
 		$packageArgs = @{
 			packageName   = $env:ChocolateyPackageName
