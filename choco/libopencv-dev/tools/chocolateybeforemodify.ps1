bddd2c32bc0b6eaca8d27ad5b3bac4bcb39a81d5
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2022-11-23 11:50:07 +0100

    Update libopencv-dev.4.2.0 package

--- choco/libopencv-dev/tools/chocolateybeforemodify.ps1
+++ choco/libopencv-dev/tools/chocolateybeforemodify.ps1
@@ -3,9 +3,39 @@
 # Source variables which are shared between install and uninstall.
 . $PSScriptRoot\sharedVars.ps1
 
-if (Test-Path "$env:SystemDrive\OpenCV$OpenCVVer") {
-    Remove-Item -Recurse -Force "$env:SystemDrive\OpenCV$OpenCVVer"
+$pp = Get-PackageParameters
+$installDir = $env:SystemDrive
+if ($pp.InstallDir -or $pp.InstallationPath) { 
+	$installDir = $pp.InstallDir + $pp.InstallationPath 
 }
+Write-Host "OpenCV$OpenCVVer is going to be uninstalled from '$installDir'"
+
+$root = Join-Path $installDir "OpenCV$OpenCVVer"
+
+$newpath = [environment]::GetEnvironmentVariable("Path","Machine")
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\mingw\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\mingw\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc8\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc8\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc9\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc9\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc10\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc10\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc11\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc11\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc12\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc12\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc14\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc14\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc15\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc15\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc16\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc16\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc17\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc17\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x86\vc18\bin" }) -join ';'
+$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$root\x64\vc18\bin" }) -join ';'
+[environment]::SetEnvironmentVariable("Path",$newpath,"Machine")
 
 if (Test-Path $CMakeRegistryPath) {
   if (Test-Path $CMakeSystemRepositoryPath\$CMakePackageName) {
@@ -13,27 +43,6 @@ if (Test-Path $CMakeRegistryPath) {
   }
 }
 
-$newpath = [environment]::GetEnvironmentVariable("Path","Machine")
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\mingw\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\mingw\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc8\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc8\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc9\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc9\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc10\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc10\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc11\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc11\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc12\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc12\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc14\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc14\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc15\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc15\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc16\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc16\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc17\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc17\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x86\vc18\bin" }) -join ';'
-$newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$env:SystemDrive\OpenCV$OpenCVVer\x64\vc18\bin" }) -join ';'
-[environment]::SetEnvironmentVariable("Path",$newpath,"Machine")
+if (Test-Path $root) {
+    Remove-Item -Recurse -Force $root
+}
--- choco/libopencv-dev/tools/chocolateyinstall.ps1
+++ choco/libopencv-dev/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $pp = Get-PackageParameters
 $installDir = $env:SystemDrive
 if ($pp.InstallDir -or $pp.InstallationPath) { 
-	$InstallDir = $pp.InstallDir + $pp.InstallationPath 
+	$installDir = $pp.InstallDir + $pp.InstallationPath 
 }
 Write-Host "OpenCV$OpenCVVer is going to be installed in '$installDir'"
 
-$root = Join-Path "$installDir" "OpenCV$OpenCVVer"
+$root = Join-Path $installDir "OpenCV$OpenCVVer"
 New-Item -ItemType Directory -Force -Path $root | Out-Null
 
 if (!$pp['url']) { 
@@ -152,4 +152,70 @@ $pathtoadd = "$root\$arch\$runtime\bin"
 if (!($pp['NoPath']) -and !([environment]::GetEnvironmentVariable("Path","Machine") -match [regex]::escape($pathtoadd))) {
 	$newpath = [environment]::GetEnvironmentVariable("Path","Machine") + ";$pathtoadd"
 	[environment]::SetEnvironmentVariable("Path",$newpath,"Machine")
+}
+
+for ($i = 1; $i -le 99; $i++) {
+	if ($pp['url'+$i]) {
+		$url = $pp['url'+$i]
+		#$checksum = $pp['sha256']
+		$packageArgs = @{
+			packageName   = $env:ChocolateyPackageName
+			unzipLocation = "$root"
+			url           = $url
+			#checksum      = $checksum
+			#checksumType  = 'sha256'
+		}
+		Install-ChocolateyZipPackage @packageArgs
+
+		# Analyze url to guess what to add to Windows PATH...
+		if ($url -match "x86") {
+			$arch = "x86"
+		}
+		else {
+			$arch = "x64"
+		}
+		if ($url -match "vc8") {
+			$runtime = "vc8"
+		}
+		if ($url -match "vc9") {
+			$runtime = "vc9"
+		}
+		elseif ($url -match "vc10") {
+			$runtime = "vc10"
+		}
+		elseif ($url -match "vc11") {
+			$runtime = "vc11"
+		}
+		elseif ($url -match "vc12") {
+			$runtime = "vc12"
+		}
+		elseif ($url -match "vc14") {
+			$runtime = "vc14"
+		}
+		elseif ($url -match "vc15") {
+			$runtime = "vc15"
+		}
+		elseif ($url -match "vc16") {
+			$runtime = "vc16"
+		}
+		elseif ($url -match "vc17") {
+			$runtime = "vc17"
+		}
+		elseif ($url -match "vc18") {
+			$runtime = "vc18"
+		}
+		else {
+			$runtime = "mingw"
+		}
+
+		if (!$pp['NoRegistry']) {
+			New-Item "$CMakeSystemRepositoryPath\$CMakePackageName" -ItemType directory -Force
+			New-ItemProperty -Name "CMakePackageDir" -PropertyType String -Value "$root" -Path "$CMakeSystemRepositoryPath\$CMakePackageName" -Force
+		}
+		$pathtoadd = "$root\$arch\$runtime\bin"
+		if (!($pp['NoPath']) -and !([environment]::GetEnvironmentVariable("Path","Machine") -match [regex]::escape($pathtoadd))) {
+			$newpath = [environment]::GetEnvironmentVariable("Path","Machine") + ";$pathtoadd"
+			[environment]::SetEnvironmentVariable("Path",$newpath,"Machine")
+		}
+	}
 }
