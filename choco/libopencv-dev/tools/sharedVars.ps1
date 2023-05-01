5b46c55e82d559825ff2f4687b577ef6fe7c4165
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2023-05-01 17:06:33 +0200

    Update libopencv-dev.4.6.0 package

--- choco/libopencv-dev/tools/sharedVars.ps1
+++ choco/libopencv-dev/tools/sharedVars.ps1
@@ -1,6 +1,6 @@
 if ((Get-ProcessorBits 32) -or $env:ChocolateyForceX86 -eq $true) { $arch = "x86" } else { $arch = "x64" }
 $MinGWMVer = "11"
-$OpenCVVer = "4.5.4"
+$OpenCVVer = "4.6.0"
 $CMakeRegistryPath = "HKCU:\SOFTWARE\Kitware\CMake"
 $CMakeSystemRepositoryPath = "HKLM:\SOFTWARE\Kitware\CMake\Packages"
 $CMakePackageName = "OpenCV"
