50f0689139fcd2bb41af807ab010829592f5daf6
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2024-05-01 16:06:08 +0200

    Revert "Temporarily disable x86 binary in aqt package since it currently causes too many false positives with antivirus software"
    This reverts commit 0045fef9ca6ba67eea55f26e3954dbe904d6cf08.

--- choco/aqt/tools/chocolateyInstall.ps1
+++ choco/aqt/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$url        = 'https://github.com/miurahr/aqtinstall/releases/download/v3.1.12/aqt_x86.exe' # Disabled due to too many virus false detections.
+$url        = 'https://github.com/miurahr/aqtinstall/releases/download/v3.1.12/aqt_x86.exe'
 $url64      = 'https://github.com/miurahr/aqtinstall/releases/download/v3.1.12/aqt_x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\aqt.exe"
-  #url           = $url
+  url           = $url
   url64bit      = $url64
-  #checksum      = '32FFBAEFE56CFED858B92FCC48B9A3DA2207178281DDFE3D75ABCCC3A1A88B0E'
-  #checksumType  = 'sha256'
+  checksum      = '32FFBAEFE56CFED858B92FCC48B9A3DA2207178281DDFE3D75ABCCC3A1A88B0E'
+  checksumType  = 'sha256'
   checksum64    = 'C07978A5CBACE54394945C67089D3736436A47F84380CE1B632D2188BC7E2CB3'
   checksumType64= 'sha256'
   forceDownload = $true
