ba1bc2440df484767b7792634a2762c495329090
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2024-12-07 12:52:10 +0100

    Add back x86 binary in aqt package

--- choco/aqt/tools/chocolateyInstall.ps1
+++ choco/aqt/tools/chocolateyInstall.ps1
@@ -1,11 +1,15 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/miurahr/aqtinstall/releases/download/v3.1.19/aqt_x86.exe'
 $url64      = 'https://github.com/miurahr/aqtinstall/releases/download/v3.1.19/aqt_x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\aqt.exe"
+  url           = $url
   url64bit      = $url64
+  checksum      = '42A8E8694375E0AD8B16C3B4D50D159ED0869313C96A0185FE442871FE03B644'
+  checksumType  = 'sha256'
   checksum64    = 'EC430FF33F5E0D096FF8AAB4A8041390D7B3B88A905C1ECFDEE26BF87475AB67'
   checksumType64= 'sha256'
   forceDownload = $true
