c702acf6a9d6a206a9da085f8cf4d5433acf75d7
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2025-03-16 12:10:50 +0100

    Temporarily remove all x86 binary references in aqt package since it currently causes too many false positives with antivirus software

--- choco/aqt/tools/chocolateyInstall.ps1
+++ choco/aqt/tools/chocolateyInstall.ps1
@@ -1,15 +1,11 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/miurahr/aqtinstall/releases/download/v3.2.1/aqt_x86.exe'
 $url64      = 'https://github.com/miurahr/aqtinstall/releases/download/v3.2.1/aqt_x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\aqt.exe"
-  url           = $url
   url64bit      = $url64
-  checksum      = '1C7DFD92E2E8FC068A12EE4C1E79A5CE41FBE4F724753B8B85C57429A90FAFF8'
-  checksumType  = 'sha256'
   checksum64    = 'F833F8240B0E741ABBEE376A443169E06F32D1A1B6EB9D6E62FF4A4A09277101'
   checksumType64= 'sha256'
   forceDownload = $true
